ee07c7c5a0b083c566396a42c178ba8ea55dabc4
Zura Japaridze <daviti.basiladze475@law.tsu.edu.ge>
2023-08-17 17:29:33 +0400

    My-Auto

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -14,7 +14,7 @@ const LoaderContainer = styled.div`
 
 const LoaderCircle = styled.div`
   border: 4px solid rgba(0, 0, 0, 0.1);
-  border-top: 4px solid #3498db;
+  border-top: 4px solid #fd4100;
   border-radius: 50%;
   width: 40px;
   height: 40px;
